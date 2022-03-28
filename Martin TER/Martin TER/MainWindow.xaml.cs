77d6261054267e3593432d825546241828f4bf12
Matthieu <matthieutrinquart2000@gmail.com>
2022-03-28 11:43:03 +0200

    mutli-page WPF application

---
+++ Martin TER/Martin TER/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Martin_TER
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string message = "coucou";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void afficher(object sender, RoutedEventArgs e)
+        {
+            if (this.affiche.Text == "")
+                this.affiche.Text = "Coucou";
+            else
+                this.affiche.Text = "";
+
+
+
+            Window1 win = new Window1();
+            win.affiche2.Text = message;
+            win.Show();
+
+
+
+
+
+
+        }
+
+        public String getMessage()
+        {
+            return message;
+        }
+    }
+}
