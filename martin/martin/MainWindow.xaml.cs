92e03d9d641a2d3af6e053437c2bfdb38b5e64ea
gaetanrm <gaetanrm@github.com>
2022-03-28 11:14:56 +0200

    adding coucou WPF app

---
+++ martin/martin/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace martin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void afficher(object sender, RoutedEventArgs e)
+        {
+            if (this.affiche.Text == "")
+                this.affiche.Text = "Coucou";
+            else
+                this.affiche.Text = "";
+
+
+
+
+        }
+
+    }
+}
