382b6f22f44b626201a6a86645438aef9861c082
Matthieu <matthieutrinquart2000@gmail.com>
2022-04-17 16:11:41 +0200

    correction application

--- Test TER/Test TER/obj/Debug/net6.0-windows/App.g.cs
+++ Test TER/Test TER/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Test_TER {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Test_TER {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             Test_TER.App app = new Test_TER.App();
             app.InitializeComponent();
